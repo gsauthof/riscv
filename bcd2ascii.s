364facb80fdb26ffa5835290914bd86da30beeaf
Georg Sauthoff <mail@georg.so>
2020-01-26 18:37:51 +0100

    Add RISC-V vectorized BCD2ASCII conversion example

---
+++ bcd2ascii.s
@@ -0,0 +1,91 @@
+# void bcd2ascii(void* dst, const void* src, size_t n)
+#
+# a0 = dst, a1 = src, a2 = n
+
+# Regarding the comments:
+# Register content is written right-to-left, starting with the
+# least-significant element - enclosed by | |.
+# Nibbles (i.e. 4 bit byte halves) are sometimes denoted by variables
+# g, h, i, j, ...
+
+# On a RISC-V "V" implementation with VLEN=128 bits, each
+# loop iteration converts 64 input bytes, i.e. 128 digits.
+# Since each iteration executes 14 instruction, it has a
+# throughput of 9 digits per instruction.
+# In contrast to that, an SSSE3 implementation with a similar number of
+# instructions only converts 8 input bytes per iteration. 
+
+# 2020, Georg Sauthoff, GPLv3+
+
+    .text                     # Start text section
+    .align 2                  # align 4 byte instructions by 2**2 bytes
+    .global bcd2ascii         # define global function symbol
+bcd2ascii:
+
+    # ## Prepare 16 element ASCII character lookup table
+
+    li a6, 16                 # load immediate (pseudo instruction)
+    vsetvli t0, a6, e8, m8    # switch to 8 bit element size,
+                              # i.e. 4 groups of 8 registers
+    # assert t0 == 16
+
+    vid.v v8                  # store Vector Element Indices,
+                              # i.e. v8 = | 16, ..., 2, 1, 0 |
+    vmsgtu.vi v0, v8, 9       # set mask-bit if greater than unsigned immediate
+    # --> v0 = | 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 |
+
+    li a7, 48                 # load immediate, i.e. '0'
+    vadd.vx v8, v8, a7        # add that scalar to each element
+
+    addi a7, a7, -9           # add immediate, i.e. set to 39 == 'a'-'0'-10,
+                              # i.e. to arrive at 'a', 'b', ...
+    vadd.vx v8, v8, a7, v0.t  # masked add for the additional offset
+
+    # The final lookup table is then:
+    # --> v8 = | 'f', ...,  'b', 'a', '9',  ... , '2', '1', '0' |
+
+    li t2, 0xf                # load immediate, mask lower nibble in each byte
+
+    # ## Main convert load, convert and store loop
+.Loop:                        # local symbol name because of .L prefix
+    vsetvli a3, a2, e16, m8   # switch to 16 bit element size,
+                              # 4 groups of 8 registers
+    # --> a3 = min(a2, 8*vlenb/2)
+    vlbu.v v16, (a1)          # Load a3 unsigned bytes,
+                              # one byte per 16 bit element, zero-extend,
+                              # starting at addr stored in a1
+    # --> v16 = | 0, a1[vlenb/2-1], ..., 0, a1[1], 0, a1[0] |, ...,
+    #     v23 = | 0, a1[a3-1],       ...,  0, a1[7*vlenb/2] |
+    # --> v16 = | ... 00mn 00kl 00ij 00gh |
+
+    add a1, a1, a3            # increment src by read elements
+    sub a2, a2, a3            # decrement n
+
+    vsll.vi v24, v16, 8       # shift-left-logical each element by 8 bits
+    # --> v24 = | ... mn00 kl00 ij00 gh00 |
+
+    vsrl.vi v16, v16, 4       # shift-right-logical each element by 4 bits
+    # --> v16 = | ... 000m 000k 000i 000g |
+
+    slli a3, a3, 1            # shift left logical by immediate,
+                              # i.e. to double the number of vector elements
+    vsetvli t4, a3, e8, m8    # switch to 8 bit element size,
+                              # 4 groups of 8 registers
+
+    vand.vx v24, v24, t2      # and each element with 0x0f,
+                              # i.e. zero-out the high nibbles
+    # --> v24 = | ... 0n 00 0l 00 0j 00 0h 00 |
+    vor.vv  v16, v16, v24     # or each element
+    # --> v16 = | ... 0n 0m 0l 0k 0j 0i 0h 0g |
+
+    # look up ASCII values
+    vrgather.vv v24, v8, v16  # vd[i] = (vs1[i] >= VLMAX) ? 0 : vs2[vs1[i]]
+    # --> v24[i] = (v16[i] >= VLMAX) ? 0 : v8[v16[i]]
+
+    vsb.v v24, (a0)           # write result to dst
+    # --> a0[0] = v24[0], a0[1] = v24[1], ..., a0[vl-1] = v24[vlenb-1], ...,
+    #     a0[vlenb*7] = v31[0],           ..., a0[a3-1] = v31[vlenb-1]
+    # --> a0[0..a3-1] = [ 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ]
+    add  a0, a0, a3           # increment dst
+    bnez a2, .Loop            # branch to loop head if not equal to zero
+    ret
---
+++ start_bcd2a.s
@@ -0,0 +1,67 @@
+    .text                     # Start text section
+    .balign 4                 # align 4 byte instructions by 4 bytes
+    .global _start            # global 
+_start:
+                              # check if vector extension is enabled
+                              # user-mode doesn't have privileges to
+                              # read mstatus/sstatus/misa CSRs
+                              # thus, unclear how to check for V support
+    li    t1, 0x1800000       # disable this check for now
+    #csrr  t1, mstatus        # control and status register, i.e. read the
+                              # mstatus register
+    li    t2, 0b11            # load immediate mask
+    slli  t2, t2, 23          # shift left logical immediate by 23 bits
+                              # because "V" draft 0.8 defines the vector
+                              # context status field VS as mstatus[24:23]
+                              # (0b00 -> off, 0b01 -> initial, 0b10 -> clean,
+                              #  0b11 -> dirty)
+    and   t3, t1, t2
+    beqz  t3, v_disabled_error
+
+                              # Prepare calling bcd2ascii()
+    addi  sp, sp, -68         # grow stack by 64+4 bytes, some additional
+                              # space but keep it 4 byte aligned
+    mv    a0, sp              # store output on stack
+    lui   a1, %hi(inp)        # load start address of
+    addi  a1, a1, %lo(inp)    # the input string
+    li    a2, 32              # load immediate: sizeof inp
+    call  bcd2ascii           # we don't need to save/restore our
+                              # return address because we don't return ...
+    li    t0, 0xa             # load immediate: newline
+    sb    t0, 64(sp)          # store byte
+                              # i.e. terminate output string with '\n'
+    li    a0, 1               # stdout
+    mv    a1, sp              # read output located on the stack
+    li    a2, 65              # i.e. 64+1 characters
+    li    a7, 64              # exit syscall number
+    ecall                     # call write(2)
+
+    li    a0, 0               # set exit status to zero
+exit:
+    li    a7, 93              # exit syscall number
+    ecall                     # call exit(2)
+1:
+    j     1b                  # loop forever in case exit failed ...
+
+v_disabled_error:
+    li    a0, 2               # stderr
+    lui   a1, %hi(err_msg)    # load error message start address
+    addi  a1, a1, %lo(err_msg)
+    lui   a2, %hi(err_msg_size)     # load error message size
+    addi  a2, a2, %lo(err_msg_size)
+    li    a7, 64              # exit syscall number
+    ecall                     # call write(2)
+    li    a0, 1               # load immediate exit argument
+    j     exit
+    
+
+    .section .rodata          # Start read-only data section
+    .balign 4                 # align to 4 bytes
+inp:
+    .byte 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef
+    .byte 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
+    .byte 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef
+    .byte 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
+err_msg:
+    .string "ERROR: RISC-V 'V' vector extension is disabled!\n"
+    .set err_msg_size, . - err_msg
