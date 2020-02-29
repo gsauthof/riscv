0f074e4e93bc206502c05ec26624aa21dfbeda19
Georg Sauthoff <mail@gms.tf>
2020-02-29 12:11:11 +0100

    Divide and Conquer the matching vector

--- memrchr.s
+++ memrchr.s
@@ -17,56 +17,52 @@ memrchr:
     sub a0, a0, a3           # decrement end pointer
     vlb.v v8, (a0)           # load a3 bytes
     vmseq.vx v0, v8, a1      # set mask bit if equal to scalar c
+
+    # since there is no vlast.m, we have to use vfirst.m and post-process
+
     vfirst.m a4, v0          # find lowest index of set mask bit
     bgez a4, .Lmatch         # branch if greater-than-or-equal-to-zero
     sub a2, a2, a3           # decrement n
     bnez a2, .Loop           # branch if not-equal to zero, i.e. continue loop
     li a0, 0                 # load-immediate NULL as return value
     ret
-
-.Lmatch:                     # since we don't have vlast.m we use vid instead
-    li a6, 256               # with vid, we can only index up to 256 elements
-                             # i.e. a register group with more than
-                             # 256 * 8 bits would overflow the index
+.Lmatch:
+    li a7, 256               # maximum vl that doesn't overflow 8bit indices
 .Loop2:
-    # minu t0, a3, a6        # requires "B"itmanip extension
-    mv t0, a3                # initialize t0 with current vl
-    ble a3, a6, 1f           # branch if less-than-or-equal-to 256
-    mv t0, a6                # otherwise set to 256, i.e. our minimum
-1:
-    vsetvli t1, t0, e8, m8   # make sure to consider the first 256 elements,
-                             # at most
+    ble a3, a7, .Ldone       # branch if less-than-or-equal-to 256,
+                             # i.e. jump is the happy case
 
-    # the code also works without limiting the vl to 256, but then we get into
-    # O(n^2) comparisons
+    # otherwise, divide & conquer the vector
 
+    srli a5, a3, 1           # divide vl by 2
+    sub a6, a3, a5           # in case vl was odd, i.e.
+                             # high_part_size = a6, low_part_size = a5
+
+                             # move the high part into v16
+    vslidedown.vx v16, v8, a5
+
+    vsetvli t0, a6, e8, m8   # ignore tailing bytes in the high_part
+
+    vmseq.vx v24, v16, a1    # check for matches in the high part
+    vfirst.m a4, v24         # store index of first match
+
+    vsetvli a3, a5, e8, m8   # update config for lower part, in case we branch
+    bltz a4, .Loop2          # branch if less-than-zero
+
+    vsetvli a3, a6, e8, m8   # restore config for higher part
+    vmv.v.v v8, v16          # vector mv high part to low part
+    vmcpy.m v0, v24          # restore mask in higher part
+    add a0, a0, a5           # increment src by size of low part
+
+    j .Loop2                 # unconditionally branch to loop head
+.Ldone:
     vid.v v16, v0.t          # write element index in each masked element
-    vredmaxu.vs v24, v16, v16, v0.t # v24[0] = max_unsigned(v16[*], v16[0])
+
+                             # v24[0] = max_unsigned(v16[*], v16[0])
+    vredmaxu.vs v24, v16, v16, v0.t
 
     vmv.x.s a5, v24          # move first vector element to register
     andi    a5, a5, 0xff     # remove sign bits in case element
-                             # was sign-extended
-    add a7, a0, a5           # move possible result into extra register
-                             
-    ble a3, a6, .Ldone       # branch if less-than-or-equal-to 256,
-                             # i.e. jump is the happy case
 
-                             # tedious case: register group has more than 256
-                             # elements, i.e. the remaining elements might contain
-                             # another match
-
-    vsetvli a3, a3, e8, m8   # restore previous config
-
-    addi a5, a5, 1           # add immediate to skip over the current maximum
-    add a0, a0, a5           # increment src
-    sub a3, a3, a5           # decrement current vl
-
-    vslidedown.vx v8, v8, a5 # move out the lowest already visited elements
-
-    vsetvli a3, a3, e8, m8   # update config
-    vmseq.vx v0, v8, a1      # set mask bit for each element equal to scalar
-    vfirst.m a4, v0          # move index of first set mask bit into register
-    bgez a4, .Loop2          # branch if greater-than-or-equal-to-zero
-.Ldone:
-    mv a0, a7                # move right-most match into ret val register
+    add a0, a0, a5
     ret
